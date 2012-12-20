71a4aa2c50ffc66d83f90ab65b2aa880ca45364a
Maurizio Pelizzone <maurizio.pelizzone@gmail.com>
2012-12-20 13:06:36 +0100

    aggiunto phar

--- utils/create-phar.php
+++ utils/create-phar.php
@@ -1,9 +1,9 @@
 <?php
 
-$phar = new Phar( "build/wpsetup.phar", 
+$phar = new Phar( "../build/wpsetup.phar", 
             FilesystemIterator::CURRENT_AS_FILEINFO |  FilesystemIterator::KEY_AS_FILENAME, 
             "wpsetup.phar");
 
-$phar["index.php"] = file_get_contents("src/index.php");
-$phar["wp-config-sample.php"] = file_get_contents("src/wp-config-sample.php");
+$phar["index.php"] = file_get_contents("../src/index.php");
+$phar["wp-config-sample.php"] = file_get_contents("../src/wp-config-sample.php");
 $phar->setStub($phar->createDefaultStub("index.php"));
